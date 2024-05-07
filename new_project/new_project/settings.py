32505ed2bab0cc0a18a1bab4f004ef478e6f9429
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2024-05-07 17:33:51 +0200

    initial commit

--- new_project/new_project/settings.py
+++ new_project/new_project/settings.py
@@ -100,11 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.MyUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
